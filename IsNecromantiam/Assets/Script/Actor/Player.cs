499931793704fc869d4672f85d943dd04cb1653b
Hawk-OR <watakkuruakunnto@gmail.com>
2026-05-26 18:39:43 +0900

    眷属召喚！
    眷属は何もせず５秒で消えます

--- IsNecromantiam/Assets/Script/Actor/Player.cs
+++ IsNecromantiam/Assets/Script/Actor/Player.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,6 +8,20 @@ public class Player : Actor
     private PlayerInput m_Input = null;
 
     [SerializeField] Transform m_Target = null;
+
+    [Header("Summon Parameter")]
+    [SerializeField] SummonParam m_SummonParam = new();
+    [System.Serializable]
+    private class SummonParam
+    {
+        public List<Object> summonPrefab = new();
+        public float radius = 2f;
+        public float speed = 3f;
+        public uint amount = 1;
+    }
+    private float m_SummonTimer = 0.0f;
+
+    [SerializeField] List<Servants> m_Servants = new();
 
     enum ControlMode
     {
@@ -46,10 +61,40 @@ public class Player : Actor
     {
         var rt = m_Input.actions.FindAction("RTrigger").ReadValue<float>();
         if (Gamepad.current != null)
+        {
             if (rt > 0.0f)
+            {
                 Gamepad.current.SetMotorSpeeds(rt, rt);
+                m_SummonTimer += Time.deltaTime;
+
+                if (m_SummonTimer >= m_SummonParam.speed)
+                {
+                    m_SummonTimer = 0.0f;
+
+                    var param = m_SummonParam;
+                    for (int i = 0; i < param.amount; i++)
+                    {
+                        var length = Random.Range(0.0f, param.radius);
+                        var vec = Vector3Extensions.AngleTo(Random.Range(0.0f, 360.0f));
+                        var pos = m_Target.position + vec * length;
+                        var spawnObj = param.summonPrefab[Random.Range(0, param.summonPrefab.Count)] as GameObject;
+                        var obj = Instantiate<GameObject>(spawnObj, pos, transform.rotation);
+                        obj.AddComponent<Servants>();
+                        m_Servants.Add(obj.GetComponent<Servants>());
+                    }
+                }
+            }
             else
+            {
                 Gamepad.current.SetMotorSpeeds(0.0f, 0.0f);
+            }
+        }
+
+        for (int i = 0; i < m_Servants.Count;)
+        {
+            if (m_Servants[i] == null) m_Servants.RemoveAt(i);
+            else i++;
+        }
 
         var targetPos = m_Target.position - transform.position;
         if (m_ControlMode == ControlMode.Target)
@@ -79,6 +124,11 @@ public class Player : Actor
         base.Update();
 
         m_Target.position = transform.position + targetPos;
+
+        Vector3 summonR = Vector3.one * m_SummonParam.radius * 2.0f;
+        summonR.y = m_Target.lossyScale.y;
+        m_Target.localScale = summonR;
+
         m_Animator.SetFloat("Move", m_Velocity.magnitude / m_Parameter.WalkSpeed);
     }
 
--- IsNecromantiam/Assets/Script/Math/Vector3Extensions.cs
+++ IsNecromantiam/Assets/Script/Math/Vector3Extensions.cs
@@ -3,4 +3,6 @@ using UnityEngine;
 public static class Vector3Extensions
 {
     public static Vector2 xz(this Vector3 v) => new Vector2(v.x, v.z);
+
+    public static Vector3 AngleTo(float angle, float y = 0.0f) => new Vector3(Mathf.Cos(angle), y, Mathf.Sin(angle));
 }
